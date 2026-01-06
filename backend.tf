82cff3bb8c3815db6bef3ab3ea86c0cd8972c5dd
premasaudagar <prema.saudagar@gmail.com>
2026-01-06 16:40:06 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "my-first-bucket-for-first-project-1"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamodb-table"
+    dynamodb_table = "my-first-dynamoDB"
   }
 }
